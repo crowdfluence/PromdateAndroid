eb2eadd9cfc6fd1b808b14de80d0a79de6dc2bbf
Loganarmack <loganarmack@gmail.com>
2019-04-05 14:44:14 -0400

    Made images circles

--- app/src/main/java/com/example/logan/promdate/SingleAdapter.kt
+++ app/src/main/java/com/example/logan/promdate/SingleAdapter.kt
@@ -1,6 +1,9 @@
 package com.example.logan.promdate
 
 import android.arch.paging.PagedListAdapter
+import android.graphics.*
+import android.graphics.drawable.BitmapDrawable
+import android.support.v4.content.ContextCompat
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
@@ -54,11 +57,59 @@ class SingleAdapter(private val clickListener: (User) -> Unit) :
 
     //sets image from url
     fun ImageView.loadUrl(url: String) {
+
         val fullUrl = "http://ec2-35-183-247-114.ca-central-1.compute.amazonaws.com${url.substring(2 until url.length)}"
         Picasso.get()
             .load(fullUrl)
+            .transform(RoundedTransformation(50, 1, ContextCompat.getColor(context, R.color.lightGray)))
+            .resize(100, 100)
+            .centerCrop()
             .placeholder(R.drawable.default_profile) //TODO: Change to loading animation
             .error(R.drawable.promdate_logo) //TODO: Change to actual error
             .into(this)
+    }
+}
+
+//converts image into rounded image with border
+class RoundedTransformation(private val radius: Int,
+                            private val margin: Int,
+                            private val borderColor: Int) : com.squareup.picasso.Transformation {
+
+    override fun transform(source: Bitmap): Bitmap {
+
+        val paint = Paint()
+        paint.isAntiAlias = true
+        paint.shader = BitmapShader(source, Shader.TileMode.CLAMP, Shader.TileMode.CLAMP)
+
+        val output = Bitmap.createBitmap(source.width, source.height, Bitmap.Config.ARGB_8888)
+        val canvas = Canvas(output)
+        canvas.drawCircle(
+            (source.width.toFloat() - margin.toFloat()) / 2f,
+            (source.height.toFloat() - margin.toFloat()) / 2f,
+            radius.toFloat() - 2f,
+            paint
+        )
+
+        if (source != output) {
+            source.recycle()
+        }
+
+        val paint1 = Paint()
+        paint1.color = borderColor
+        paint1.style = Paint.Style.STROKE
+        paint1.isAntiAlias = true
+        paint1.strokeWidth = 2f
+        canvas.drawCircle(
+            (source.width.toFloat() - margin.toFloat()) / 2f,
+            (source.height.toFloat() - margin.toFloat()) / 2f,
+            radius.toFloat() - 2f,
+            paint1
+        )
+
+        return output
+    }
+
+    override fun key(): String {
+        return "rounded"
     }
 }
