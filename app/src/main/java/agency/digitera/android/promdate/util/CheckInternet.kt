5fe84571eae15f9572c0d0b9944036f31b63817c
Victor <victor.a.vicari@gmail.com>
2019-10-03 08:59:06 -0300

    Replaced the singleton(check internet) for a context extension function. Still can be accessed from all around the app, but is far more elegant and kotlin friendly

--- app/src/main/java/agency/digitera/android/promdate/util/CheckInternet.kt
+++ app/src/main/java/agency/digitera/android/promdate/util/CheckInternet.kt
@@ -3,12 +3,8 @@ package agency.digitera.android.promdate.util
 import android.content.Context
 import android.net.ConnectivityManager
 
-class CheckInternet {
-    companion object {
-        fun isNetworkAvailable(context: Context): Boolean {
-            val connectivityManager = context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-            val activeNetworkInfo = connectivityManager.activeNetworkInfo
-            return activeNetworkInfo != null && activeNetworkInfo.isConnected
-        }
-    }
+fun Context.isNetworkAvailable(): Boolean {
+    val connectivityManager = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetworkInfo = connectivityManager.activeNetworkInfo
+    return activeNetworkInfo != null && activeNetworkInfo.isConnected
 }
